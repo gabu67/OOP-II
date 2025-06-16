520df30b397b00372a6745dec17e8972f915fe01
Gabriel Osugo Kabese <gabriel.kabese@strathmore.edu>
2025-06-16 20:52:13 +0300

    New tasks

---
+++ ArrayReverse.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        String[] pallets = { "B14", "A11", "B12", "A13" };
+        System.out.println("Sorted ....");
+        Arrays.sort(pallets);
+
+        for (String pallet : pallets) {
+            System.out.println("-- " + pallet);
+        }
+
+        System.out.println("Reversed ....");
+        List<String> palletList = Arrays.asList(pallets);
+        Collections.reverse(palletList);
+        for (String pallet : palletList) {
+            System.out.println("-- " + pallet);
+        }
+    }
+}
